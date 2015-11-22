261a7271e189467f68500d834fdf1edf2fb29f1b
alexvassel <alexvassel@gmail.com>
2015-11-22 19:24:40 +0300

    crud forms

---
+++ rambler/forms.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from django import forms
+
+from rambler.models import Poll, Question, Answer
+
+
+class PollForm(forms.ModelForm):
+    class Meta:
+        model = Poll
+        exclude = ['slug', 'user']
+
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        exclude = ['poll']
+
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        exclude = ['question']
